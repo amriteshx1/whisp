184130ebb7eaf9cfaada4eecd5df085b83bc985f
amriteshx1 <amriteshx18@gmail.com>
2025-07-07 18:33:28 +0530

    add message send route and finalize msg router structure

--- backend/src/routes/msgRoutes.ts
+++ backend/src/routes/msgRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth";
-import { getMsg, getUsers, markSeen } from "../controllers/msgController";
+import { getMsg, getUsers, markSeen, sendMsg } from "../controllers/msgController";
 
 const msgRouter = express.Router();
 
 msgRouter.get("/users", protectRoute, getUsers);
 msgRouter.get("/:id", protectRoute, getMsg);
-msgRouter.get("mark/:id", protectRoute, markSeen);
+msgRouter.get("/mark/:id", protectRoute, markSeen);
+msgRouter.post("/send/:id", protectRoute, sendMsg);
 
 export default msgRouter;
