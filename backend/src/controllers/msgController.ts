ae8994c23099dfb5251579c4dcfa14c19e1cd75b
amriteshx1 <amriteshx18@gmail.com>
2025-07-07 17:06:11 +0530

    get users with unseen message count excluding logged-in user

---
+++ backend/src/controllers/msgController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import User from "../models/user";
+import Message from "../models/message";
+import { IUser } from "../models/user";
+
+interface AuthRequest extends Request {
+  user?: IUser;
+}
+
+// Get all users except logged-in user, along with unseen message count
+export const getUsers = async (req: AuthRequest, res: Response) => {
+  try {
+    const userID = req.user!._id;
+
+    const allUsers = await User.find({ _id: { $ne: userID } }).select("-password");
+
+    const unseenMsg: Record<string, number> = {};
+
+    const promises = allUsers.map(async (user) => {
+      const msgs = await Message.find({
+        senderId: user._id,
+        receiverId: userID,
+        seen: false,
+      });
+
+      if (msgs.length > 0) {
+        unseenMsg[user._id.toString()] = msgs.length;
+      }
+    });
+
+    await Promise.all(promises);
+
+    res.json({ success: true, users: allUsers, unseenMsg });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
