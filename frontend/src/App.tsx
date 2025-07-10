9dfd120560858a763bf3ab260e916d5ee12fc031
amriteshx1 <amriteshx18@gmail.com>
2025-07-10 18:00:00 +0530

    add route for homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
 import ObserverProvider from "./components/ObserverProvider";
+import LandingPage from "./pages/LandingPage";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
     </ObserverProvider>
