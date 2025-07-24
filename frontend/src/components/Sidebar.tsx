f5638e23ed20c901207510fee3bcea2eb1ff4d20
amriteshx1 <amriteshx18@gmail.com>
2025-07-24 20:54:04 +0530

    made some styling tweaks in sidebar for better ui

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -18,7 +18,7 @@ const Sidebar = ({selectedUser, setSelectedUser}: SidebarProps) => {
   return (
     <div className={`h-full p-5 rounded-r-xl overflow-y-auto text-gray-900 ${selectedUser ? "max-md:hidden" : ''}`}>
       <div className="pb-5">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center pl-[1vh]">
             <img src={applogo} alt="whispLogo" className="h-[5vh]" />
             <div className="relative py-2 group">
                 <img src={menuIcon} alt="menu" className="max-h-6 cursor-pointer" />
@@ -40,13 +40,13 @@ const Sidebar = ({selectedUser, setSelectedUser}: SidebarProps) => {
       <div className="flex flex-col gap-3">
         {userDummyData.map((user, index) => (
           <div onClick={() => {setSelectedUser(user)}}
-          key={index} className={`relative flex items-center gap-4 p-2 pl-4 rounded cursor-pointer max-sm:text-sm hover:bg-sky-100 hover:rounded-xl ${selectedUser && selectedUser._id === user._id && 'bg-[#282142]/50'}`}>
+          key={index} className={`relative flex items-center gap-4 p-2 pl-4 rounded cursor-pointer max-sm:text-sm hover:bg-sky-100 hover:rounded-xl ${selectedUser && selectedUser._id === user._id && 'bg-sky-200 rounded-xl'}`}>
             <img src={user?.profilePic || assets.avatar_icon} alt={user.fullName} className="w-[40px] aspect-[1/1] rounded-full" />
             <div className="flex flex-col leading-5">
-              <p className="text-sm font-medium text-zinc-600">{user.fullName}</p>
+              <p className="text-sm font-medium text-zinc-700">{user.fullName}</p>
               {
                 index < 3
-                ? <span className="text-green-400 text-xs">Online</span>
+                ? <span className="text-green-600 text-xs">Online</span>
                 : <span className="text-neutral-400 text-xs">Offline</span>
               }
             </div>
