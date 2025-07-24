a4c34cf2393e17afbb041ec5966e00d0ba0d97d4
amriteshx1 <amriteshx18@gmail.com>
2025-07-24 20:54:41 +0530

    add login and profile routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ObserverProvider from "./components/ObserverProvider";
 import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
       </BrowserRouter>
     </ObserverProvider>
