e21300c180031376cab862df695059ef85cef458
amriteshx1 <amriteshx18@gmail.com>
2025-07-25 20:58:41 +0530

    add route guards based on authUser using Navigate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,27 @@
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ObserverProvider from "./components/ObserverProvider";
 import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import { Toaster } from "react-hot-toast";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const App = () => {
+
+  const { authUser } = useContext(AuthContext);
+
   return (
     <ObserverProvider>
       <BrowserRouter>
       <Toaster />
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/home" element={authUser ? <HomePage /> : <Navigate to={"/"} />} />
+          <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+          <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to={"/"} />} />
         </Routes>
       </BrowserRouter>
     </ObserverProvider>
