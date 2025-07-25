129297217cc28379840d727035183a7e9282788b
amriteshx1 <amriteshx18@gmail.com>
2025-07-25 20:41:34 +0530

    add updateProfile function to handle user data updates

--- frontend/context/AuthContext.tsx
+++ frontend/context/AuthContext.tsx
@@ -58,6 +58,20 @@ export const AuthProvider = ({ children }) => {
         socket.disconnect();
     }
 
+    //update profile
+    const updateProfile = async (body)=>{
+        try {
+            const { data } = await axios.put("/api/auth/update-profile", body);
+            if(data.success){
+                setAuthUser(data.user);
+                toast.success("Profile updated successfully")
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+
     
 
     useEffect(()=>{
