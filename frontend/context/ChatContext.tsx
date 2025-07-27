cdd7e9e11b9a16a413de53e1c022b870d3cb20c9
amriteshx1 <amriteshx18@gmail.com>
2025-07-27 09:21:48 +0530

    add real-time message listener with subscribeToMessages and mark messages as seen

--- frontend/context/ChatContext.tsx
+++ frontend/context/ChatContext.tsx
@@ -53,6 +53,24 @@ export const ChatProvider = ({ children }) => {
         }
     }
 
+    //subscribe to msgs for selected user
+    const subscribeToMessages = async () => {
+        if(!socket) return;
+
+        socket.on("newMessage", (newMessage)=>{
+            if(selectedUser && newMessage.senderId === selectedUser._id){
+                newMessage.seen = true;
+                setMessages((prevMessages)=>[...prevMessages, newMessage]);
+                axios.put(`/api/messages/mark/${newMessage._id}`);
+            }else{
+                setUnseenMessages((prevUnseenMessages) =>({
+                    ...prevUnseenMessages, [newMessage.senderId] :
+                    prevUnseenMessages[newMessage.senderId] ? prevUnseenMessages[newMessage.senderId] + 1 : 1
+                }))
+            }
+        })
+    }
+
     
 
     const value = {
