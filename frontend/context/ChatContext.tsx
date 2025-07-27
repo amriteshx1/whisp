c77c99c81be5fca52473b31fa79b47bb742bd161
amriteshx1 <amriteshx18@gmail.com>
2025-07-27 09:19:30 +0530

    add getUsers function to fetch users and unseen messages from backend

--- frontend/context/ChatContext.tsx
+++ frontend/context/ChatContext.tsx
@@ -1,4 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import toast from "react-hot-toast";
 
 
 export const ChatContext = createContext();
@@ -6,9 +8,29 @@ export const ChatContext = createContext();
 export const ChatProvider = ({ children }) => {
 
     const [messages, setMessages] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [unseenMessages, setUnseenMessages] = useState({});
+
+    const {socket, axios} = useContext(AuthContext);
+
+    //get all users
+    const getUsers = async () => {
+        try {
+            const { data } = await axios.get("/api/messages/users");
+            if(data.success){
+                setUsers(data.users);
+                setUnseenMessages(data.unseenMessages);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    
 
     const value = {
-
+        messages, users, selectedUser, getUsers, setMessages, sendMsg, setSelectedUser, unseenMessages, setUnseenMessages 
     }
 
     return (
