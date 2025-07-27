a63b56787246bcd7e10ae3a772f81d1ec17cd5b8
amriteshx1 <amriteshx18@gmail.com>
2025-07-27 14:51:07 +0530

    refactored HomePage to use selectedUser from ChatContext instead of local state

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,26 +1,26 @@
 
-import { useState } from "react";
+import { useContext } from "react";
 import ChatBox from "../components/ChatBox";
 import Sidebar from "../components/Sidebar";
 import UserBox from "../components/UserBox";
-import type { User } from "../assets/chat-app-assets/assets";
+import { ChatContext } from "../../context/ChatContext";
 
 const HomePage = () => {
 
-    const [selectedUser, setSelectedUser] = useState<User | false>(false);
+  const {selectedUser} = useContext(ChatContext);
 
   return (
     <div className="min-h-screen w-full relative overflow-auto">
         <div
-          className="absolute inset-0 z-0 h-full w-full px-[15vh] py-[5vh]"
+          className="absolute inset-0 z-0 h-full w-full px-[5vh] py-[5vh]"
           style={{
             background: "radial-gradient(125% 125% at 50% 90%, #fff 40%, #90caff 100%)",
           }}
         >
-            <div className={`h-full grid grid-cols-1 relative ${selectedUser ? 'grid-cols-[1fr_2fr_1fr]' : 'grid-cols-2' }`}>
-                <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
-                <ChatBox selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
-                <UserBox selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+            <div className={`h-full grid grid-cols-1 relative ${selectedUser ? 'grid-cols-[1.3fr_2.5fr_1fr]' : 'grid-cols-2' }`}>
+                <Sidebar />
+                <ChatBox />
+                <UserBox />
 
             </div>
     
