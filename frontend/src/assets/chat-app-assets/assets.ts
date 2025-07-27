b7937fdbaf8ac42e947472c853645f49e95f5296
amriteshx1 <amriteshx18@gmail.com>
2025-07-27 17:08:12 +0530

    add all core visual assets including logos, icons, and UI images

---
+++ frontend/src/assets/chat-app-assets/assets.ts
@@ -0,0 +1,29 @@
+import avatar_icon from './avatar_icon.png';
+import gallery_icon from './gallery_icon.png';
+import help_icon from './help_icon.png';
+import logo_icon from './logo_icon.svg';
+import logo_big from './logo_big.svg';
+import logo from './logo.png';
+import search_icon from './search_icon.png';
+import send_button from './send_button.png';
+import menu_icon from './menu_icon.png';
+import arrow_icon from './arrow_icon.png';
+import code from './code.svg';
+
+// optional but useful type for asset object
+const assets: Record<string, string> = {
+  avatar_icon,
+  gallery_icon,
+  help_icon,
+  logo_big,
+  logo_icon,
+  logo,
+  search_icon,
+  send_button,
+  menu_icon,
+  arrow_icon,
+  code,
+};
+
+export default assets;
+
