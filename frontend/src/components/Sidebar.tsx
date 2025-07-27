971397d72193bfc48fd237c4aee1ba7fbd668048
amriteshx1 <amriteshx18@gmail.com>
2025-07-27 10:55:49 +0530

    replace dummy data with real users, online status, and unseenMsg in sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -3,10 +3,11 @@ import { useNavigate } from "react-router-dom";
 import applogo from "../assets/appLogo.png";
 import menuIcon from "../assets/menuIcon.png";
 import searchIcon from "../assets/searchIcon.png";
-import assets, { userDummyData } from "../assets/chat-app-assets/assets";
+import assets from "../assets/chat-app-assets/assets";
 import type { User } from "../assets/chat-app-assets/assets";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import { ChatContext } from "../../context/ChatContext";
 
 type SidebarProps = {
   selectedUser: User | false;
@@ -14,11 +15,22 @@ type SidebarProps = {
 };
 
 
-const Sidebar = ({selectedUser, setSelectedUser}: SidebarProps) => {
+const Sidebar = () => {
 
-   const { logout } = useContext(AuthContext);
+   const {getUsers, users, selectedUser, setSelectedUser, unseenMessages, setUnseenMessages} = useContext(ChatContext);
+
+   const { logout, onlineUsers } = useContext(AuthContext);
+
+   const [input, setInput] = useState(false);
 
     const navigate = useNavigate();
+
+    const filteredUsers = input ? users.filter((user)=>user.fullName.toLowerCase().includes(input.toLowerCase())) : users;
+
+    useEffect(()=>{
+      getUsers();
+    }, [onlineUsers])
+
   return (
     <div className={`h-full p-5 rounded-r-xl overflow-y-auto text-gray-900 ${selectedUser ? "max-md:hidden" : ''}`}>
       <div className="pb-5">
@@ -36,26 +48,26 @@ const Sidebar = ({selectedUser, setSelectedUser}: SidebarProps) => {
     
         <div className="bg-sky-200 rounded-full flex items-center gap-2 py-3 px-4 mt-5">
             <img src={searchIcon} alt="search" className="h-[2.5vh]" />
-            <input type="text" className="bg-transparent border-none outline-none text-gray-700 text-xs flex-1" placeholder="Search User..." />
+            <input onChange={(e)=>setInput(e.target.value)} type="text" className="bg-transparent border-none outline-none text-gray-700 text-xs flex-1" placeholder="Search User..." />
         </div>
 
       </div>
 
       <div className="flex flex-col gap-3">
-        {userDummyData.map((user, index) => (
+        {filteredUsers.map((user, index) => (
           <div onClick={() => {setSelectedUser(user)}}
           key={index} className={`relative flex items-center gap-4 p-2 pl-4 rounded cursor-pointer max-sm:text-sm hover:bg-sky-100 hover:rounded-xl ${selectedUser && selectedUser._id === user._id && 'bg-sky-200 rounded-xl'}`}>
             <img src={user?.profilePic || assets.avatar_icon} alt={user.fullName} className="w-[40px] aspect-[1/1] rounded-full" />
             <div className="flex flex-col leading-5">
               <p className="text-sm font-medium text-zinc-700">{user.fullName}</p>
               {
-                index < 3
+                onlineUsers.includes(user._id)
                 ? <span className="text-green-600 text-xs">Online</span>
                 : <span className="text-neutral-400 text-xs">Offline</span>
               }
             </div>
-            {index > 2 && <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-sky-300">
-            {index}</p> }
+            {unseenMessages[user._id] > 0 && <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-sky-300">
+              {unseenMessages[user._id]}</p> }
           </div>
         ))}
 
