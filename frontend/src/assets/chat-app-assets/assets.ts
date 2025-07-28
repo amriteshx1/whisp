9c1f7b7e0924b3b43e48a3be14477be100571cee
amriteshx1 <amriteshx18@gmail.com>
2025-07-28 16:47:08 +0530

    removed imports that were using deleted assets

--- frontend/src/assets/chat-app-assets/assets.ts
+++ frontend/src/assets/chat-app-assets/assets.ts
@@ -1,12 +1,7 @@
 import avatar_icon from './avatar_icon.png';
 import gallery_icon from './gallery_icon.png';
 import help_icon from './help_icon.png';
-import logo_icon from './logo_icon.svg';
-import logo_big from './logo_big.svg';
-import logo from './logo.png';
-import search_icon from './search_icon.png';
 import send_button from './send_button.png';
-import menu_icon from './menu_icon.png';
 import arrow_icon from './arrow_icon.png';
 import code from './code.svg';
 
@@ -15,15 +10,9 @@ const assets: Record<string, string> = {
   avatar_icon,
   gallery_icon,
   help_icon,
-  logo_big,
-  logo_icon,
-  logo,
-  search_icon,
   send_button,
-  menu_icon,
   arrow_icon,
   code,
 };
 
 export default assets;
-
