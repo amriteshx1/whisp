64c2eb0c39e52fdc2fdaf53d1c229ffc3d0f4348
amriteshx1 <amriteshx18@gmail.com>
2025-08-02 18:52:14 +0530

    add respond endpoint for friend requests

--- backend/src/routes/friendRoutes.ts
+++ backend/src/routes/friendRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { sendFriendRequest } from "../controllers/friendController";
+import { sendFriendRequest, respondToFriendRequest } from "../controllers/friendController";
 import { protectRoute } from '../middleware/auth';
 
 const friendRouter = express.Router();
 
 // send friend request
 friendRouter.post("/request", protectRoute, sendFriendRequest);
+friendRouter.post("/respond", protectRoute, respondToFriendRequest);
 
 export default friendRouter;
