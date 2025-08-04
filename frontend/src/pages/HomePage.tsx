ba2d268956e335601285a7356cc5f533d93ba069
amriteshx1 <amriteshx18@gmail.com>
2025-08-04 18:27:00 +0530

    updated homepage background

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -10,12 +10,9 @@ const HomePage = () => {
   const {selectedUser} = useContext(ChatContext);
 
   return (
-    <div className="min-h-screen w-full relative overflow-auto">
+    <div className="min-h-screen w-full relative bg-black overflow-auto">
         <div
           className="absolute inset-0 z-0 h-full w-full px-[5vh] py-[5vh]"
-          style={{
-            background: "radial-gradient(125% 125% at 50% 90%, #fff 40%, #90caff 100%)",
-          }}
         >
             <div className={`h-full grid grid-cols-1 relative ${selectedUser ? 'grid-cols-[1.3fr_2.5fr_1fr]' : 'grid-cols-2' }`}>
                 <Sidebar />
