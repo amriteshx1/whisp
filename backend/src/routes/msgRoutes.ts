1d7a40a24cec0caf775b133b2b647c4fba8a4c89
amriteshx1 <amriteshx18@gmail.com>
2025-08-09 10:20:39 +0530

    add route for getting non-friends users

--- backend/src/routes/msgRoutes.ts
+++ backend/src/routes/msgRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth";
-import { getMsg, getUsers, markSeen, sendMsg } from "../controllers/msgController";
+import { getMsg, getUsers, markSeen, sendMsg, getNonFriends } from "../controllers/msgController";
 
 const msgRouter = express.Router();
 
 msgRouter.get("/users", protectRoute, getUsers);
+msgRouter.get("/nonFriends", protectRoute, getNonFriends);
 msgRouter.get("/:id", protectRoute, getMsg);
 msgRouter.get("/mark/:id", protectRoute, markSeen);
 msgRouter.post("/send/:id", protectRoute, sendMsg);
