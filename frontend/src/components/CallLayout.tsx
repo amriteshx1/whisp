7816c5b33e0e82af579970dff58f56a58d6a2b86
amriteshx1 <amriteshx18@gmail.com>
2025-08-11 21:43:02 +0530

    implemented ui for showing incoming calls

--- frontend/src/components/CallLayout.tsx
+++ frontend/src/components/CallLayout.tsx
@@ -40,7 +40,22 @@ const CallLayout: React.FC = () => {
     endCall();
   };
 
-  
+  if (incomingCall && !callActive) {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70">
+      <div className="bg-neutral-900 p-5 rounded-xl text-white flex flex-col gap-4 items-center">
+        <p className="text-lg font-semibold">Incoming Call</p>
+        <p className="text-sm text-gray-400">User {incomingCall.from} is calling…</p>
+        <div className="flex gap-4 mt-4">
+          <button onClick={rejectCall} className="bg-gradient-to-tl from-neutral-950 via-red-500/50 to-red-600 px-4 py-2 rounded-full cursor-pointer hover:bg-red-950"><img src={assets.reject} alt="" className="h-5 w-5" /></button>
+          <button onClick={acceptCall} className="bg-gradient-to-tl from-neutral-950 via-green-400/20 to-green-500 px-4 py-2 rounded-full cursor-pointer hover:bg-green-950"><img src={assets.accept} alt="" className="h-5 w-5" /></button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
 return null;
 };
 
