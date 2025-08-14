049b42ca41eeda4178123f32c283281eb0d95ec9
amriteshx1 <amriteshx18@gmail.com>
2025-08-14 19:49:37 +0530

    add google & its callback route for redirection

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { checkAuth, login, signup, updateProfile } from '../controllers/userController';
 import { protectRoute } from '../middleware/auth';
+import passport from "passport";
+import { generateToken } from "../lib/utils";
 
 const userRouter = express.Router();
 
@@ -8,5 +10,23 @@ userRouter.post("/signup", signup);
 userRouter.post("/login", login);
 userRouter.put("/updateProfile", protectRoute, updateProfile);
 userRouter.get("/check", protectRoute, checkAuth);
+
+userRouter.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+userRouter.get(
+  "/google/callback",
+  passport.authenticate("google", { session: false, failureRedirect: `${process.env.FRONTEND_URL}/login?oauth=failed` }),
+  (req, res) => {
+    // @ts-ignore
+    const userId = req.user._id.toString();
+    const token = generateToken(userId);
+
+    const redirectUrl = `${process.env.FRONTEND_URL}/home?token=${encodeURIComponent(token)}`;
+    res.redirect(redirectUrl);
+  }
+);
 
 export default userRouter;
