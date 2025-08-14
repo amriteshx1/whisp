2a2c1b3e9c1ca669c838c0f269b5239d46ce9a7a
amriteshx1 <amriteshx18@gmail.com>
2025-08-14 22:47:48 +0530

    add route for talking with bot

--- backend/src/routes/msgRoutes.ts
+++ backend/src/routes/msgRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth";
-import { getMsg, getUsers, markSeen, sendMsg, getNonFriends } from "../controllers/msgController";
+import { getMsg, getUsers, markSeen, sendMsg, getNonFriends, talkToBot } from "../controllers/msgController";
 
 const msgRouter = express.Router();
 
@@ -9,5 +9,6 @@ msgRouter.get("/nonFriends", protectRoute, getNonFriends);
 msgRouter.get("/:id", protectRoute, getMsg);
 msgRouter.put("/mark/:id", protectRoute, markSeen);
 msgRouter.post("/send/:id", protectRoute, sendMsg);
+msgRouter.post("/chat-bot", talkToBot);
 
 export default msgRouter;
