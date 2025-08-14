7a892c113f87cf18c6ecd78f518f334f90d25171
amriteshx1 <amriteshx18@gmail.com>
2025-08-14 19:48:11 +0530

    add passport initialization in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,8 @@ import userRouter from './routes/userRoutes';
 import msgRouter from './routes/msgRoutes';
 import friendRouter from './routes/friendRoutes';
 import { Server } from 'socket.io';
+import passport from "passport";
+import "./lib/passport";
 
 //create express and http server
 const app = express();
@@ -112,6 +114,7 @@ io.on("connection", (socket) => {
 //middleware setup
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
+app.use(passport.initialize());
 
 //routes setup
 app.use("/api/auth", userRouter);
