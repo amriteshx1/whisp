0d2dc6bfcee74e978814a66df7d19ca2cd002a07
amriteshx1 <amriteshx18@gmail.com>
2025-08-14 19:45:12 +0530

    add Express type declaration for 'req.user'

---
+++ backend/src/types/express/index.d.ts
@@ -0,0 +1,10 @@
+import { IUser } from "../../models/user";
+
+declare global {
+  namespace Express {
+    interface User extends IUser {} 
+    interface Request {
+      user?: IUser; 
+    }
+  }
+}
