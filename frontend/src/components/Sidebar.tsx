5d371e511b3b4e6083622c3542f0395d6cbe41fe
amriteshx1 <amriteshx18@gmail.com>
2025-08-16 17:35:07 +0530

    some minor ui tweaks in Sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -58,7 +58,7 @@ const Sidebar = () => {
 
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
-            if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+            if (isMenuOpen && menuRef.current && !menuRef.current.contains(event.target as Node)) {
                 setIsMenuOpen(false);
             }
 
@@ -108,10 +108,10 @@ const Sidebar = () => {
             <div className="relative py-2" ref={menuRef}>
                 <img src={menuIcon} alt="menu" className="max-h-6 cursor-pointer" onClick={() => setIsMenuOpen(!isMenuOpen)} />
                 {isMenuOpen && (
-                <div className="absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-neutral-900 text-white/80 block">
+                <div className="absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-neutral-800 text-white/80 block">
                     <p onClick={() => {navigate('/friends'); setIsMenuOpen(false); }} className="cursor-pointer text-sm">Add Friends</p>
                     <hr className="my-2 border-t border-white/80" />
-                    <p onClick={() => {navigate('/profile'); setIsMenuOpen(false);}} className="cursor-pointer text-sm">Edit Profile</p>
+                    <p onClick={() => {navigate('/profile'); setIsMenuOpen(false); }} className="cursor-pointer text-sm">Edit Profile</p>
                     <hr className="my-2 border-t border-white/80" />
                     <p onClick={() => {logout(); setIsMenuOpen(false);}} className="cursor-pointer text-sm">Logout</p>
                 </div>
@@ -131,9 +131,12 @@ const Sidebar = () => {
         className="relative flex items-center gap-4 p-2 pl-4 rounded cursor-pointer hover:bg-neutral-900/70 hover:rounded-xl mb-3"
       >
         <img src={assets.bot || assets.avatar_icon} alt="Chat Bot" className="w-[40px] aspect-[1/1] p-1 rounded-full" />
+        <div className="flex justify-between items-center w-full">
         <div className="flex flex-col leading-5">
-          <p className="text-sm font-medium text-sky-200 flex items-center gap-1">Whisp Bot <img src={assets.verified} alt="whisp bot" className="h-4 w-4" /></p>
-          <span className="text-neutral-500 text-xs">AI Assistant</span>
+          <p className="text-sm font-semibold text-sky-200 flex items-center gap-1">Whisp Bot <img src={assets.verified} alt="whisp bot" className="h-4 w-4" /></p>
+          <span className="text-neutral-500 text-xs font-medium">AI Assistant</span>
+        </div>
+        <img src={assets.pin} alt="" className="h-4 w-4" />
         </div>
       </div>
 
@@ -144,10 +147,9 @@ const Sidebar = () => {
               .map((_, i) => (
                 <div
                   key={i}
-                  className="flex items-center gap-4 p-2 pl-4 rounded-xl animate-pulse bg-neutral-900/40"
+                  className="flex items-center gap-4 p-2 pl-4 rounded-xl animate-pulse"
                 >
-                  <div className="w-[40px] h-[40px] bg-neutral-700 rounded-full flex items-center justify-center text-lg">
-                    👤
+                  <div className="w-[40px] h-[40px] bg-neutral-600 rounded-full flex items-center justify-center text-lg">
                   </div>
                   <div className="flex flex-col gap-2 flex-1">
                     <div className="w-3/5 h-3 bg-neutral-700 rounded"></div>
@@ -160,18 +162,18 @@ const Sidebar = () => {
           key={index} className={`relative flex items-center gap-4 p-2 pl-4 rounded cursor-pointer max-sm:text-sm hover:bg-neutral-900/70 hover:rounded-xl ${selectedUser && selectedUser._id === user._id && 'bg-neutral-900/70 rounded-xl'}`}>
             <img src={user?.profilePic || assets.avatar_icon} alt={user.fullName} className="w-[40px] aspect-[1/1] rounded-full" />
             <div className="flex flex-col leading-5">
-              <p className="text-sm font-medium text-white/80">{user.fullName}</p>
+              <p className="text-sm font-semibold text-white/80">{user.fullName}</p>
               {
                 onlineUsers.includes(user._id)
-                ? <span className="text-green-700 text-xs">Online</span>
-                : <span className="text-neutral-500 text-xs">Offline</span>
+                ? <span className="text-green-700 text-xs font-medium">Online</span>
+                : <span className="text-neutral-500 text-xs font-medium">Offline</span>
               }
             </div>
             {unseenMessages[user._id] > 0 && <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-gradient-to-tl from-neutral-950 via-white/10 to-neutral-700 text-white/75">
               {unseenMessages[user._id]}</p> }
           </div>
         ))}
-
+        <p className="text-center text-xs text-neutral-600 mt-[5vh]">Explore the Friends tab to grow your network!</p>
       </div>
 
       {/* BOT OVERLAY */}
