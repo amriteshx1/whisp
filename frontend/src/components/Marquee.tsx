d22463468b55efc6844004f72f80eb2993aa8599
amriteshx1 <amriteshx18@gmail.com>
2025-08-18 18:19:29 +0530

    made landing page responsive for tablets

--- frontend/src/components/Marquee.tsx
+++ frontend/src/components/Marquee.tsx
@@ -3,8 +3,8 @@ import Marquee from "react-fast-marquee";
 
 const FeatureMarquee = () => {
   return (
-    <div className="relative w-full bg-black p-[1vh] px-[15vh] mt-[15vh]">
-      <h2 className="text-center text-[3vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">
+    <div className="relative w-full bg-black p-[1vh] lg:px-[15vh] px-[5vh] lg:mt-[15vh] mt-[5vh]">
+      <h2 className="text-center lg:text-[3vw] text-[1.8vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">
         What Powers Your Presence
       </h2>
 
@@ -16,7 +16,7 @@ const FeatureMarquee = () => {
         pauseOnHover={true}
         className="text-neutral-700 text-[1rem] font-medium"
       >
-        <div className="flex gap-[7vh] px-[2.5vh] text-[1.5vw] mt-[7vh] bg-gradient-to-tl from-neutral-950 via-white/70 to-neutral-700 bg-clip-text text-transparent hover:cursor-default">
+        <div className="flex lg:gap-[7vh] gap-[3vh] px-[2.5vh] lg:text-[1.5vw] text-[1.15vh] lg:mt-[7vh] mt-[3vh] bg-gradient-to-tl from-neutral-950 via-white/70 to-neutral-700 bg-clip-text text-transparent hover:cursor-default">
           <span>⚡ Speed</span>
           <span>🔒 Privacy</span>
           <span>☁️ Cloud Uploads</span>
--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -19,24 +19,24 @@ const LandingPage = () => {
   return (
       
 <div className="h-screen w-full relative overflow-auto bg-black">
-        <div className="flex justify-between items-center text-neutral-500 py-[1.5vh] px-[3vh] mx-[15vh] rounded-[100px] border-b-2 border-b-neutral-800">
-            <div className='w-[40%] flex justify-start items-center'>
-                <p className='text-[1.6vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent'>Whisp.</p>
+        <div className="flex justify-between items-center text-neutral-500 lg:py-[1.5vh] lg:px-[3vh] lg:mx-[15vh] py-[1vh] px-[3vh] mx-[5vh] rounded-[100px] border-b-2 border-b-neutral-800">
+            <div className='lg:w-[40%] w-[20%] flex justify-start items-center'>
+                <p className='lg:text-[1.6vw] text-[1.5vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent'>Whisp.</p>
             </div>
 
-            <div className='w-[50%] flex gap-[4vh] justify-end items-center'>
-                <p className='text-[1.05vw] cursor-pointer font-medium hover:text-neutral-400'>About</p>
-                <p className='text-[1.05vw] font-medium cursor-pointer hover:text-neutral-400'>Features</p>
-                <p className='text-[1.05vw] font-medium cursor-pointer hover:text-neutral-400'>How it works</p>
-                <button onClick={handleClick} className='block text-[1.05vw] font-medium cursor-pointer hover:text-neutral-400'>Sign in</button>
+            <div className='lg:w-[50%] w-[70%] flex lg:gap-[4vh] gap-[3vh] justify-end items-center'>
+                <p className='lg:text-[1.05vw] text-[1.1vh] cursor-pointer font-medium hover:text-neutral-400'>About</p>
+                <p className='lg:text-[1.05vw] text-[1.1vh] font-medium cursor-pointer hover:text-neutral-400'>Features</p>
+                <p className='lg:text-[1.05vw] lg:block hidden text-[1vh] font-medium cursor-pointer hover:text-neutral-400'>How it works</p>
+                <button onClick={handleClick} className='block lg:text-[1.05vw] text-[1.1vh] font-medium cursor-pointer hover:text-neutral-400'>Sign in</button>
                 <button onClick={handleClick}
-                className='text-[0.9vw] text-white/75 font-medium py-[0.5vh] px-[1.5vh] rounded-4xl bg-neutral-900 cursor-pointer hover:text-white/90'>Get Started</button>
+                className='lg:text-[0.9vw] text-[0.95vh] text-white/75 font-medium lg:py-[0.5vh] lg:px-[1.5vh] py-[0.4vh] px-[1.2vh] rounded-4xl bg-neutral-900 cursor-pointer hover:text-white/90'>Get Started</button>
             </div>
         </div>
 
         <div className="min-h-[85vh] flex flex-col justify-start items-center p-[1vh]">
             <div className="flex flex-col justify-center items-center mt-[6vh] pt-[1vh] text-zinc-700">
-              <div className="flex justify-center items-center gap-3 py-[1vh] px-[2vh] text-[0.8vw] border border-neutral-800 rounded-full text-white/75">
+              <div className="flex justify-center items-center gap-3 lg:py-[1vh] lg:px-[2vh] py-[0.7vh] px-[1vh] lg:text-[0.8vw] text-[0.85vh] border border-neutral-800 rounded-full text-white/75">
                 <p>✔ Instant Messaging</p>
                 <p>✔ End-to-End Encryption</p>
                 <p>✔ Crystal-Clear Calls</p>
@@ -45,22 +45,22 @@ const LandingPage = () => {
                   initial={{ y: 15, opacity: 0, scale: 0.9 }}
                   animate={{ y: 0, opacity: 1, scale: 1 }}    
                   transition={{ duration: 0.4, ease: "easeOut" }}
-                  className="text-[4.5vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Conversations That Feel Close</motion.p>
+                  className="lg:text-[4.5vw] text-[3.5vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Conversations That Feel Close</motion.p>
                 <motion.p 
                   initial={{ y: 15, opacity: 0, scale: 0.9 }}
                   animate={{ y: 0, opacity: 1, scale: 1 }}    
                   transition={{ duration: 0.4, ease: "easeOut", delay: 0.1 }}
-                  className="text-[4.5vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Even From Afar</motion.p>
+                  className="lg:text-[4.5vw] text-[3.5vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Even From Afar</motion.p>
                 <motion.p 
                   initial={{ y: 15, opacity: 0, scale: 0.9 }}
                   animate={{ y: 0, opacity: 1, scale: 1 }}    
                   transition={{ duration: 0.4, ease: "easeOut", delay: 0.2 }}
-                  className="text-[1.5vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Stay connected — softly, silently, seamlessly.</motion.p>
+                  className="lg:text-[1.5vw] text-[1.3vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Stay connected — softly, silently, seamlessly.</motion.p>
                 <motion.button 
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}    
                   transition={{ duration: 0.8, ease: "easeOut", delay: 0.4 }}
-                  onClick={handleClick} className="text-[1.2vw] bg-gradient-to-tl from-neutral-950 via-white/10 to-neutral-700 px-[2.5vh] py-[1vh] rounded-3xl cursor-pointer text-white/75 mt-5 hover:text-white/90 ">Step into Whisp</motion.button>
+                  onClick={handleClick} className="lg:text-[1.2vw] text-[1vh] bg-gradient-to-tl from-neutral-950 via-white/10 to-neutral-700 lg:px-[2.5vh] lg:py-[1vh] px-[1.3vh] py-[0.7vh] rounded-3xl cursor-pointer text-white/75 mt-5 hover:text-white/90 ">Step into Whisp</motion.button>
             </div>
 
             <div className="flex justify-center items-center w-screen  relative">
@@ -70,69 +70,69 @@ const LandingPage = () => {
 
             <FeatureMarquee />
 
-            <div className="flex flex-col justify-center items-center mt-[20vh] mb-[10vh] w-full mx-[15vh]">
-                <h2 className="text-[3vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Why Whisp?</h2>
-                <div className="flex justify-around items-center mt-[7vh] text-center">
+            <div className="flex flex-col justify-center items-center lg:mt-[20vh] mt-[8vh] lg:mb-[10vh] mb-[5vh] w-full lg:mx-[15vh] mx-[5vh]">
+                <h2 className="lg:text-[3vw] text-[1.8vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Why Whisp?</h2>
+                <div className="flex justify-around items-center lg:mt-[7vh] mt-[3vh] text-center">
 
-                    <div className="flex h-[30vh] bg-neutral-900 w-[25%] flex-col justify-around items-center p-[4vh] rounded-3xl">
-                        <p className="text-[1.5vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">🔐 Private by Design</p>
-                        <p className="text-[1.2vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Messages are end-to-end protected. Your convos stay yours.</p>
+                    <div className="flex lg:h-[30vh] h-[15vh] bg-neutral-900 w-[25%] flex-col justify-around items-center lg:p-[4vh] p-[1.5vh] rounded-3xl">
+                        <p className="lg:text-[1.5vw] text-[1.15vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">🔐 Private by Design</p>
+                        <p className="lg:text-[1.2vw] text-[1vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Messages are end-to-end protected. Your convos stay yours.</p>
                     </div>
-                    <div className="flex h-[30vh] bg-neutral-900 w-[25%] flex-col justify-around items-center p-[4vh] rounded-3xl">
-                        <p className="text-[1.5vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">⚡ Real-time, Always</p>
-                        <p className="text-[1.2vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Send, receive, seen — all in a blink. Zero lag, no reloads.</p>
+                    <div className="flex lg:h-[30vh] h-[15vh] bg-neutral-900 w-[25%] flex-col justify-around items-center lg:p-[4vh] p-[1.5vh] rounded-3xl">
+                        <p className="lg:text-[1.5vw] text-[1.15vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">⚡ Real-time, Always</p>
+                        <p className="lg:text-[1.2vw] text-[1vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Send, receive, seen — all in a blink. Zero lag, no reloads.</p>
                     </div>
-                    <div className="flex h-[30vh] bg-neutral-900 w-[25%] flex-col justify-around items-center p-[4vh] rounded-3xl">
-                        <p className="text-[1.5vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">☁️ Share Moments</p>
-                        <p className="text-[1.2vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Effortlessly upload and view images. Cloudinary-powered.</p>
+                    <div className="flex lg:h-[30vh] h-[15vh] bg-neutral-900 w-[25%] flex-col justify-around items-center lg:p-[4vh] p-[1.5vh] rounded-3xl">
+                        <p className="lg:text-[1.5vw] text-[1.15vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">☁️ Share Moments</p>
+                        <p className="lg:text-[1.2vw] text-[1vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Effortlessly upload and view images. Cloudinary-powered.</p>
                     </div>
 
                 </div>
 
             </div>
 
-            <div className="h-screen w-full overflow-auto relative bg-black">
+            <div className="lg:h-screen h-[55vh] w-full overflow-auto relative bg-black">
               
-              <div className="flex flex-col justify-center items-center mt-[10vh] w-full">
-                <h2 className="text-[3vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Let your conversations begin.</h2>
-                <button onClick={handleClick} className="text-[1.4vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/10 to-neutral-700 px-[7vh] py-[1vh] rounded-xl cursor-pointer text-white/75 hover:text-white/90 mt-[5vh]">Get Started</button>
+              <div className="flex flex-col justify-center items-center lg:mt-[10vh] mt-[5vh] w-full">
+                <h2 className="lg:text-[3vw] text-[1.8vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent">Let your conversations begin.</h2>
+                <button onClick={handleClick} className="lg:text-[1.4vw] text-[1.05vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/10 to-neutral-700 lg:px-[7vh] px-[2vh] lg:py-[1vh] py-[0.6vh] rounded-xl cursor-pointer text-white/75 hover:text-white/90 lg:mt-[5vh] mt-[2vh]">Get Started</button>
               </div>
 
-              <div className="flex flex-col w-full mt-[20vh]">
+              <div className="flex flex-col w-full lg:mt-[20vh] mt-[10vh]">
 
                 <div className=" w-full flex justify-around items-center">
                     <div className="w-[30%] flex flex-col justify-start items-center">
-                        <p className="text-[1.2vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[2vh]">Company</p>
-                        <p className="text-[0.9vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">About Us</p>
-                        <p className="text-[0.9vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Our Services</p>
-                        <p className="text-[0.9vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Privacy Policy</p>
-                        <p className="text-[0.9vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Affiliate Program</p>
+                        <p className="lg:text-[1.2vw] text-[0.95vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent lg:mb-[2vh] mb-[1.5vh]">Company</p>
+                        <p className="lg:text-[0.9vw] text-[0.8vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">About Us</p>
+                        <p className="lg:text-[0.9vw] text-[0.8vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Our Services</p>
+                        <p className="lg:text-[0.9vw] text-[0.8vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Privacy Policy</p>
+                        <p className="lg:text-[0.9vw] text-[0.8vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Affiliate Program</p>
                     </div>
 
                     <div className="w-[30%] flex flex-col justify-start items-center">
-                        <p className="text-[1.2vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[2vh]">Product</p>
-                        <p className="text-[0.9vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Whisp Web</p>
-                        <p className="text-[0.9vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Whisp Mobile</p>
-                        <p className="text-[0.9vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Premium</p>
-                        <p className="text-[0.9vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Starter Kit</p>
+                        <p className="lg:text-[1.2vw] text-[0.95vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent lg:mb-[2vh] mb-[1.5vh]">Product</p>
+                        <p className="lg:text-[0.9vw] text-[0.8vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Whisp Web</p>
+                        <p className="lg:text-[0.9vw] text-[0.8vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Whisp Mobile</p>
+                        <p className="lg:text-[0.9vw] text-[0.8vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Premium</p>
+                        <p className="lg:text-[0.9vw] text-[0.8vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Starter Kit</p>
                     </div>
 
                     <div className="w-[30%] flex flex-col justify-start items-center">
-                        <p className="text-[1.2vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[2vh]">Support</p>
-                        <p className="text-[0.9vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">FAQ</p>
-                        <p className="text-[0.9vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Feedback</p>
-                        <p className="text-[0.9vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Terms of Use</p>
-                        <p className="text-[0.9vw] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Contact</p>
+                        <p className="lg:text-[1.2vw] text-[0.95vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent lg:mb-[2vh] mb-[1.5vh]">Support</p>
+                        <p className="lg:text-[0.9vw] text-[0.8vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">FAQ</p>
+                        <p className="lg:text-[0.9vw] text-[0.8vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Feedback</p>
+                        <p className="lg:text-[0.9vw] text-[0.8vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Terms of Use</p>
+                        <p className="lg:text-[0.9vw] text-[0.8vh] bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[0.5vh]">Contact</p>
                     </div>
                 </div>
 
-                <div className="w-full flex flex-col justify-center items-center mt-[10vh]">
-                    <p className="text-[1.2vw] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent mb-[2vh]">Follow Us</p>
-                    <div className="flex justify-center items-center gap-[4vh] mt-[1vh]">
-                        <img src={instagram} alt="insta-logo" className="h-[3vh] object-cover" />
-                        <img src={discord} alt="discord-logo" className="h-[3vh] object-cover" />
-                        <img src={github} alt="github-logo" className="h-[3vh] object-cover" />
-                        <img src={twitter} alt="twitter-logo" className="h-[3vh] object-cover" />
+                <div className="w-full flex flex-col justify-center items-center lg:mt-[10vh] mt-[5vh]">
+                    <p className="lg:text-[1.2vw] text-[0.95vh] font-medium bg-gradient-to-tl from-neutral-950 via-white/80 to-neutral-700 bg-clip-text text-transparent lg:mb-[2vh] mb-[1vh]">Follow Us</p>
+                    <div className="flex justify-center items-center lg:gap-[4vh] gap-[2vh] lg:mt-[1vh] mt-[0.5vh]">
+                        <img src={instagram} alt="insta-logo" className="lg:h-[3vh] h-[1.5vh] object-cover" />
+                        <img src={discord} alt="discord-logo" className="lg:h-[3vh] h-[1.5vh] object-cover" />
+                        <img src={github} alt="github-logo" className="lg:h-[3vh] h-[1.5vh] object-cover" />
+                        <img src={twitter} alt="twitter-logo" className="lg:h-[3vh] h-[1.5vh] object-cover" />
 
                     </div>
 
